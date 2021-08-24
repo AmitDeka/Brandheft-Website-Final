e12d748eead972ee77431a38e751b5981406bfb3
AmitDeka <amitdeka49@gmail.com>
2021-08-24 06:40:42 +0530

    Update 24 Aug 2020

---
+++ public/index.js
@@ -0,0 +1,20 @@
+// Nav toggle 
+document.getElementById("hamburger").onclick = function toggleMenu() {
+    const navToggle = document.getElementsByClassName("toggle");
+    for (let i = 0; i < navToggle.length; i++) {
+        navToggle.item(i).classList.toggle("hidden");
+    }
+};
+
+// on scroll 
+function scrollHeader() {
+    const nav = document.getElementById('navBar')
+    if (this.scrollY >= 50) {
+        nav.classList.add('shadow-md')
+        nav.classList.remove('shadow-sm')
+    } else {
+        nav.classList.remove('shadow-md')
+        nav.classList.add('shadow-sm')
+    }
+}
+window.addEventListener('scroll', scrollHeader)
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        txtcolor: '#0026242',
+        primary: '#d9d0d0',
+        secondary: '#e59500',
+        red: '#840032',
+
+        fb: '#4267b2',
+        insta: '#bc2a8d',
+        twitter: '#1da1f2',
+        linkedin: '#0077b5'
+      },
+
+      fontFamily: {
+        futuraL: "'Futura Lt BT', serif",
+        futuraM: "'Futura Md BT', sans-serif",
+        futuraB: "'Futura BdCn BT', serif",
+
+      }
+    },
+  },
+  variants: {
+    extend: {
+      scrollBehavior: ['motion-safe']
+    },
+  },
+  plugins: [
+    require('tailwind-scroll-behavior')(),
+  ],
+}
