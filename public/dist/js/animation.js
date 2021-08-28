d29db25e4140d8ca92a7ceea8f8644c15329cdf2
AmitDeka <amitdeka49@gmail.com>
2021-08-28 19:36:30 +0530

    Upload for 28 Aug , 2021

--- public/dist/js/animation.js
+++ public/dist/js/animation.js
@@ -1,27 +1,20 @@
-// import { gsap , Tw} from "gsap";
-import { gsap } from "gsap/dist/gsap";
-
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
-
-
 gsap.to(".overlay h1", {
     duration: 2,
+    delay: .1,
     opacity: 0,
     y: "-100%",
     ease: Expo.easeIn
 })
 gsap.to(".overlay span", {
     duration: 2,
-    delay: .4,
+    delay: .3,
     opacity: 0,
     y: "-100%",
     ease: Expo.easeIn
 })
 gsap.to(".overlay", {
-    duration: 2,
-    delay: .8,
+    duration: 3,
+    delay: .4,
     y: "-100%",
     ease: Expo.easeInOut
 })
--- public/dist/js/index.js
+++ public/dist/js/index.js
@@ -2,26 +2,26 @@
 
 // import { gsap , Tw} from "gsap";
 
-gsap.to(".overlay", {
-    duration: 2,
-    delay: .8,
-    y: "-100%",
-    ease: Expo.easeInOut
-})
+// gsap.to(".overlay", {
+//     duration: 2,
+//     delay: .8,
+//     y: "-100%",
+//     ease: Expo.easeInOut
+// })
 
-gsap.to(".overlay h1", {
-    duration: 2,
-    opacity: 0,
-    y: "-100%",
-    ease: Expo.easeIn
-})
-gsap.to(".overlay span", {
-    duration: 2,
-    delay: .4,
-    opacity: 0,
-    y: "-100%",
-    ease: Expo.easeIn
-})
+// gsap.to(".overlay h1", {
+//     duration: 2,
+//     opacity: 0,
+//     y: "-100%",
+//     ease: Expo.easeIn
+// })
+// gsap.to(".overlay span", {
+//     duration: 2,
+//     delay: .4,
+//     opacity: 0,
+//     y: "-100%",
+//     ease: Expo.easeIn
+// })
 
 
 
