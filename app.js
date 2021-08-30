5ba951fd5c4083c5d060fa65bc50761965f35c7b
AmitDeka <amitdeka49@gmail.com>
2021-08-30 19:41:34 +0530

    Update for 30 Aug, 2021 v2

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
+// // require('dotenv').config();
+// // const express = require('express');
+// // const expressLayouts = require('express-ejs-layouts');
+// // const route = require('./route/route'); nbb                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+
+// // const app = express();
+
+// // app.use(express.static('public'));
+
+// // app.use(expressLayouts);
+// // app.set('view engine', 'ejs');
+
+
+// // app.use('/', route);
+
+// // // server lister 
+// // const PORT = process.env.PORT || 5050;
+
+// // app.listen(PORT, console.log(`Server running on 127.0.0.1:${PORT}`));
+
 // require('dotenv').config();
 // const express = require('express');
 // const expressLayouts = require('express-ejs-layouts');
-// const route = require('./route/route'); nbb                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+
 
 // const app = express();
 
+// // static folder 
 // app.use(express.static('public'));
+// app.use('/css', express.static(__dirname + 'public/dist/css'));
 
+// // set view engine
 // app.use(expressLayouts);
 // app.set('view engine', 'ejs');
 
+// // parser 
+// app.use(express.urlencoded({ extended: true }));
 
-// app.use('/', route);
+// // Router 
+// app.use('/', require('./route/route'));
+// // app.use('/auth', require('./route/auth'));
+// // app.use('/dashboard', require('./route/dashboard'));
 
-// // server lister 
-// const PORT = process.env.PORT || 5050;
+// // Passport
 
-// app.listen(PORT, console.log(`Server running on 127.0.0.1:${PORT}`));
-
-require('dotenv').config();
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-
-
-const app = express();
-
-// static folder 
-app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/dist/css'));
-
-// set view engine
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
-
-// parser 
-app.use(express.urlencoded({ extended: true }));
-
-// Router 
-app.use('/', require('./route/route'));
-// app.use('/auth', require('./route/auth'));
-// app.use('/dashboard', require('./route/dashboard'));
-
-// Passport
-
-// listen to port 
-const port = process.env.PORT || 5000;
-app.listen(port, () =>
-    console.log(`Server running on localhost:${port}`)
-)
+// // listen to port 
+// const port = process.env.PORT || 5000;
+// app.listen(port, () =>
+//     console.log(`Server running on localhost:${port}`)
+// )
--- public/dist/js/animation.js
+++ public/dist/js/animation.js
@@ -1,3 +1,6 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Overlay 
 gsap.to(".overlay h1", {
     duration: 2,
     delay: .1,
@@ -13,8 +16,83 @@ gsap.to(".overlay span", {
     ease: Expo.easeIn
 })
 gsap.to(".overlay", {
-    duration: 3,
+    duration: 2,
     delay: .4,
     y: "-100%",
     ease: Expo.easeInOut
 })
+
+// Hero section 
+gsap.from("#hero_data", {
+    opacity: 0,
+    duration: 1,
+    delay: 1,
+    x: 90,
+    ease: Expo.easeIn
+})
+
+gsap.from(".section_title", {
+    y: 30,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".section_title",
+        start: "top 90%",
+        end: "center 15%",
+        // markers: true,
+        toggleActions: "play reverse restart reverse"
+    }
+})
+
+gsap.from(".section_headline", {
+    y: 30,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".section_title",
+        start: "top 90%",
+        end: "center 15%",
+        // markers: true,
+        toggleActions: "play reverse restart reverse"
+    }
+})
+
+gsap.from(".section_footer", {
+    y: 30,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".section_title",
+        start: "top 90%",
+        end: "center 15%",
+        // markers: true,
+        toggleActions: "play reverse restart reverse"
+    }
+})
+
+gsap.from(".section_image", {
+    y: 30,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".section_title",
+        start: "top 90%",
+        end: "center 15%",
+        // markers: true,
+        toggleActions: "play reverse restart reverse"
+    }
+})
+
+gsap.from(".section", {
+    y: 30,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".section_title",
+        start: "top 90%",
+        end: "center 15%",
+        // markers: true,
+        toggleActions: "play reverse restart reverse"
+    }
+})
+
