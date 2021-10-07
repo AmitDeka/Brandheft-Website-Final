2be2bde2ce0ec25faa592d9f20fe22cd42478fe7
AmitDeka <amitdeka49@gmail.com>
2021-10-07 22:30:55 +0530

    bug fixes 7 oct, 2021

--- public/dist/js/animation.js
+++ public/dist/js/animation.js
@@ -1,27 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// Overlay 
-gsap.to(".overlay h1", {
-    duration: 2,
-    delay: .8,
-    opacity: 0,
-    y: "-100%",
-    ease: Expo.easeIn
-})
-gsap.to(".overlay span", {
-    duration: 2,
-    delay: 1,
-    opacity: 0,
-    y: "-100%",
-    ease: Expo.easeIn
-})
-gsap.to(".overlay", {
-    duration: 2,
-    delay: 1.2,
-    y: "-100%",
-    ease: Expo.easeIn
-})
-
 // Hero section 
 gsap.from(".heroText", {
     opacity: 0,
--- public/dist/js/index.js
+++ public/dist/js/index.js
@@ -1,3 +1,26 @@
+// Overlay 
+gsap.to(".overlay h1", {
+    duration: 2,
+    delay: .8,
+    opacity: 0,
+    y: "-100%",
+    ease: Expo.easeIn
+})
+gsap.to(".overlay span", {
+    duration: 2,
+    delay: 1,
+    opacity: 0,
+    y: "-100%",
+    ease: Expo.easeIn
+})
+gsap.to(".overlay", {
+    duration: 2,
+    delay: 1.2,
+    y: "-100%",
+    ease: Expo.easeIn
+})
+
+
 // Nav toggle 
 document.getElementById("hamburger").onclick = function toggleMenu() {
     const navToggle = document.getElementsByClassName("toggle");
